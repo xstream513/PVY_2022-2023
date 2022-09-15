7c39a695f2d2c1c6fa479fabe45a5162deff7561
Jan Sýkora <jan.sykora@educanet.cz>
2022-09-15 15:01:22 +0200

    přidán HelloWorld + .gitignore
    * přidána testovací třída HelloWorld
    * adresář .idea přidán do .gitignore

---
+++ src/cz/educanet/pvy/basics/HelloWorld.java
@@ -0,0 +1,62 @@
+package cz.educanet.pvy.basics;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+class HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world.");
+    printExtraMessage();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  public static void printExtraMessage() {
+    System.out.println();
+    System.out.println("*".repeat(40));
+
+    String currentDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    System.out.println("Today is: " + currentDate);
+
+    DayOfWeek currentDay = LocalDateTime.now().getDayOfWeek();
+    if (currentDay.equals(DayOfWeek.THURSDAY)) {
+      System.out.println("Today we are learning Java.");
+    } else {
+      System.out.println("Today we are not learning Java.");
+    }
+
+    System.out.println("*".repeat(40));
+  }
+
+}
